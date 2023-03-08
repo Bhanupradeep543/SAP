9bac3c0b16506dac66a1ba4dbf5b1eaa023f48cd
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2023-03-08 12:02:46 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -2,8 +2,6 @@
 # coding: utf-8
 
 # In[ ]:
-
-
 import pandas as pd
 import pickle
 from sklearn import preprocessing
@@ -16,21 +14,65 @@ import streamlit as st
 import io
 # choosing the image for application background directly from web URL
 st.markdown(f"""<style>.stApp {{                        
-             background: url("https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F5%2F59%2FSAP_2011_logo.svg%2F1200px-SAP_2011_logo.svg.png&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FSAP&tbnid=PV_gS_R4dwZfaM&vet=12ahUKEwjL9YyEifn6AhVz_jgGHayaBbsQMygAegUIARDfAQ..i&docid=403En1J1CPN8VM&w=1200&h=593&q=SAP&ved=2ahUKEwjL9YyEifn6AhVz_jgGHayaBbsQMygAegUIARDfAQ");
+             background: url("https://www.intouch-quality.com/hubfs/quality-defects-ft-lg.jpg");
              background-size: cover}}
          </style>""",unsafe_allow_html=True)
 st.write("""# SEIL SAP Notification Dashboard """) # Tittle addition
-st.subheader("Select the Options Below")
-
-# adding upload button for giving test data input to model
+st.subheader("Select the date range for notifications") 
+d = st.date_input("From", )
+e = st.date_input("TO", )
+st.subheader("Select the Planner group for obtaining repeated notifications")
+options = st.multiselect('Select the planner Group',['CIA','CIB','CIC','CID','CIN','CIV','CNI','EAP','EBP','EBR','MAP','MBP','MBM','MTM'])
+c=options[0]
+st.write(c)
 data_file = st.file_uploader("Upload SAP EXCEL file",type=['xlsx'])
-
-if st.button("Process"):
+if st.button("upload"):
     if data_file is not None:
+        data = pd.DataFrame(pd.read_excel(data_file))
+        data=data[data['Main WorkCtr']!='OPRN']
+        data1=data[data['Description'].str.contains('PM ')]
+        data=data.drop(data[data['Description'].isin(data1['Description'])].index)
         file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
-        data_file.to_csv ("sap.csv", index = None, header=True)
-        data=pd.DataFrame(pd.read_csv("sap.csv"))
-        st.subheader('total data points')
-        st.dataframe(data.shape)
-
-
+        st.subheader('Total notifications')
+        st.write(data.shape[0])
+        #st.subheader("Max. notifications Reported by")
+        #st.bar_chart(data['Reported by'].value_counts().head(10))
+        #st.subheader("Max. notifications Planner group wise")
+        #st.bar_chart(data['Planner group'].value_counts().head(7))
+        #st.subheader("User status of notification")
+        #st.write(data['User status'].value_counts().head())
+        st.subheader("Repeated notifications Planner group wise")
+        b=data[data['Planner group']==c]
+        
+        rp=b['System'].value_counts().head(20)
+        st.write(rp)
+        column_name = 'System'
+        word_counts = data[column_name].value_counts()
+        repeated_words = word_counts[word_counts > 15]
+        grouped = data.groupby(column_name)
+        repeated_rows = grouped.apply(lambda x: x[x[column_name].isin(repeated_words.index)])
+        st.subheader("Top 100 repeated notifications from SEIL P1")
+        rp1=repeated_rows['System'].value_counts().head(100)
+        st.write(rp1)
+        #def convert_df(df):
+          #return df.to_csv().encode('utf-8')
+        #cs = convert_df(repeated_rows) 
+        #adding a download button to download csv file
+        #st.download_button(label="Download",data=cs,file_name='Repeated notifications.csv',mime='text/csv')
+        for i in range(b.shape[0]):
+          plngrp['Created On']=pd.to_datetime(pd.Series(plngrp['Created On']))
+          plngrp['Created On']=plngrp['Created On'].dt.strftime('%Y/%m')
+        st.bar_chart(plngrp['Created On'].value_counts().head(30))
+        data['Created On']=pd.to_datetime(data['Created On']).dt.date
+        newdata=pd.DataFrame()
+        for i in range(data.shape[0]):
+          if (data['Created On'][i]>=d) and (data['Created On'][i]<=e) :
+            newdata=newdata.append(data.iloc[i])
+        st.subheader("Repeated notifications in mentioned dates")
+        a=newdata.iloc[:,13].value_counts().head(300)
+        st.write(a)
+        def convert_df(df):
+          return df.to_csv().encode('utf-8')
+        csv = convert_df(a) # calling the function to convert the output file into CSV
+        #adding a download button to download csv file
+        st.download_button(label="Download",data=csv,file_name='Repeated notifications.csv',mime='text/csv')
