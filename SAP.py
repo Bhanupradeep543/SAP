3107583202389110a398b7a45f83d760e9e893fc
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2023-03-12 11:11:23 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -67,7 +67,7 @@ g=options[0]
 st.write(g)
 dict={'IDF-2A':'20-HNC10','IDF-1A':'20-HNC10'}
 data2=data[data['Functional Loc.'].str.contains(dict[g])]
-
+data2 = data2.iloc[:, [3,4,5,10,114,15,18]].copy()
 st.write(data2)
 cs = convert_df(data2) 
 #adding a download button to download csv file
