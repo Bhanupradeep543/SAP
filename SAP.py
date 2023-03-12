b4afc423cdd7d8d66b9e47592bbdb2d6850cf516
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2023-03-12 10:02:02 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -28,36 +28,36 @@ data1=data[data['Description'].str.contains('PM ')]
 data=data.drop(data[data['Description'].isin(data1['Description'])].index)
 st.subheader('Total notifications')
 st.subheader(data.shape[0])
-column_name = 'System'
-word_counts = data[column_name].value_counts()
-repeated_words = word_counts[word_counts > 15]
-grouped = data.groupby(column_name)
-repeated_rows = grouped.apply(lambda x: x[x[column_name].isin(repeated_words.index)])
-st.subheader("Top 100 repeated notifications from SEIL P1")
-rp1=repeated_rows['System'].value_counts().head(100)
-st.write(rp1)
-def convert_df(df):
- return df.to_csv().encode('utf-8')
-cs = convert_df(repeated_rows) 
+#column_name = 'System'
+#word_counts = data[column_name].value_counts()
+#repeated_words = word_counts[word_counts > 15]
+#grouped = data.groupby(column_name)
+#repeated_rows = grouped.apply(lambda x: x[x[column_name].isin(repeated_words.index)])
+#st.subheader("Top 100 repeated notifications from SEIL P1")
+#rp1=repeated_rows['System'].value_counts().head(100)
+#st.write(rp1)
+#def convert_df(df):
+# return df.to_csv().encode('utf-8')
+#cs = convert_df(repeated_rows) 
 #adding a download button to download csv file
-st.download_button(label="Download",data=cs,file_name='Repeated notifications.csv',mime='text/csv')
-st.subheader("Select the Planner group for obtaining repeated notifications")
-options = st.multiselect('Select the planner Group',['CIA','CIB','CIC','CID','CIN','CIV','CNI','EAP','EBP','EBR','MAP','MBP','MBM','MTM'])
-c=options[0]
-st.write(c)
+#st.download_button(label="Download",data=cs,file_name='Repeated notifications.csv',mime='text/csv')
+#st.subheader("Select the Planner group for obtaining repeated notifications")
+#options = st.multiselect('Select the planner Group',['CIA','CIB','CIC','CID','CIN','CIV','CNI','EAP','EBP','EBR','MAP','MBP','MBM','MTM'])
+#c=options[0]
+#st.write(c)
 #st.subheader("Max. notifications Reported by")
 #st.bar_chart(data['Reported by'].value_counts().head(10))
 #st.subheader("Max. notifications Planner group wise")
 #st.bar_chart(data['Planner group'].value_counts().head(7))
 #st.subheader("User status of notification")
 #st.write(data['User status'].value_counts().head())
-st.subheader("Repeated notifications Planner group wise")
-b=data[data['Planner group']==c]
-rp=b['System'].value_counts().head(20)
-st.write(rp)
-cs = convert_df(rp) 
+#st.subheader("Repeated notifications Planner group wise")
+#b=data[data['Planner group']==c]
+#rp=b['System'].value_counts().head(20)
+#st.write(rp)
+#cs = convert_df(rp) 
 #adding a download button to download csv file
-st.download_button(label="Download",data=cs,file_name='Repeated notifications.csv',mime='text/csv')
+#st.download_button(label="Download",data=cs,file_name='Repeated notifications.csv',mime='text/csv')
 st.subheader("Select the Equipment")
 options = st.multiselect('Select the planner Group',['IDF-1A','IDF-1B','IDF-2A','IDF-2B','FDF-1A','FDF-1B','FDF-2A','FDF-2B','PAF-1A','PAF-1B','PAF-2A','PAF-2B'
                                                      ,'APH-1A','APH-1B','APH-2A','APH-2B','MILL-1A','MILL-2A','MILL-1B','MILL-1C'
