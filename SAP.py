865bdc6a00eb1b56d34666128fc6476315f454a4
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2023-03-20 11:41:21 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -81,7 +81,8 @@ cs = convert_df(data2)
 st.download_button(label="Download",data=cs,file_name='Repeated notifications.csv',mime='text/csv')
 st.subheader("TOP 5 repeated defects in the above equipment")
 rp=data2['System'].value_counts().head(5)
-st.write(rp)
+if g=="U1 sooblowing system" or "U2 sooblowing system":
+  st.write(rp)
 
 #adding a download button to download csv file
 
