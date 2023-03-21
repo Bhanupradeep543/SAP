7afe73288db294a013c5f55fcc4657589095c5f9
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2023-03-21 11:48:09 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -82,7 +82,7 @@ st.download_button(label="Download",data=cs,file_name='Repeated notifications.cs
 st.subheader("TOP 5 repeated defects in the above equipment")
 rp=data2['System'].value_counts().head(5)
 if g=='U1 sootblowing system':
-    st.write("hello0")
+    st.write("hello")
 
 #adding a download button to download csv file
 
