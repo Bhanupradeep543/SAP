b307a522f9b33affcf485d08600767a2f3973fcb
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2023-04-05 07:50:32 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -60,7 +60,12 @@ def convert_df(df):
 #adding a download button to download csv file
 #st.download_button(label="Download",data=cs,file_name='Repeated notifications.csv',mime='text/csv')
 st.subheader("select the Month and year")
-options=st.multiselect('select month',['1','2','3','4','5','6','7','8','9','10','11','12'])
+yr = st.slider('Select the month', value=0,step=1)
+min_value = 2016
+max_value = 2022
+step_value = 1
+mn = st.slider('Select the year', min_value, max_value, step_value)
+st.write('You selected:', value)
 st.subheader("Select the Equipment/System")
 options = st.multiselect('Select the planner Group',['IDF-1A','IDF-1B','IDF-2A','IDF-2B','FDF-1A','FDF-1B','FDF-2A','FDF-2B','PAF-1A','PAF-1B','PAF-2A','PAF-2B'
                                                      ,'APH-1A','APH-1B','APH-2A','APH-2B','MILL-1A','MILL-2A','MILL-1B','MILL-1C'
