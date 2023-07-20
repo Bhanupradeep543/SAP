7223c607d29ffb17f9c970c1c74e939d89008b9c
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2023-07-20 19:56:32 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -18,14 +18,35 @@ st.markdown(f"""<style>.stApp {{
              background: url("https://www.intouch-quality.com/hubfs/quality-defects-ft-lg.jpg");
              background-size: cover}}
          </style>""",unsafe_allow_html=True)
-st.write("""# SEIL SAP Notifications """) # Tittle addition
+st.write("""SEIL SAP Notifications """) # Tittle addition
 #st.write('Select the analysis Areawise /Equipment wise')zzz
 options = st.multiselect("""#Select the Area/System/Equipment""",['Area Wise','Equipment wise'])
 h=options[0]
 if h=='Area Wise':
   st.write('area wise')
+  options = st.multiselect('Select the Area',['BLR-1','BLR-2','TG-1','TG-2'])
+  g=options[0]
+  dict={'BLR-1':['10-HNC10','10-HNC20','10-HLB10','10-HLB20','10-HFD10','10-HFD20','10-HLD10','10-HLD20','10-HFV10','10-HFV20','10-HFV30','10-HFV40','10-HFV50','10-HFV60','10-HFV70','10-HFV80']
+      ,'BLR-2':['20-HNC10','20-HNC20','20-HLB10','20-HLB20','20-HFD10','20-HFD20','20-HLD10','20-HLD20','20-HFV10','20-HFV20','20-HFV30','20-HFV40','20-HFV50','20-HFV60','20-HFV70','20-HFV80']
+      ,'TG-1':'','TG-2':''}
+  
 elif h=='Equipment wise':
   st.write ('Equipment wise')
+  options = st.multiselect('Select the Equipment',['IDF-1A','IDF-1B','IDF-2A','IDF-2B','FDF-1A','FDF-1B','FDF-2A','FDF-2B','PAF-1A','PAF-1B','PAF-2A','PAF-2B'
+                                                     ,'APH-1A','APH-1B','APH-2A','APH-2B','MILL-1A','MILL-2A','MILL-1B','MILL-1C'
+                                                    ,'MILL-2B','MILL-2C','MILL-1D','MILL-2D','MILL-2E','MILL-1E','MILL-1F','MILL-2F'
+                                                    ,'MILL-2G','MILL-1G','TDBFP-1A','TDBFP-1B','TDBFP-2A','TDBFP-2B','MDBFP','U1 sootblowing system','U2 sootblowing system'])
+  g=options[0]
+  dict={'IDF-2A':'20-HNC10','IDF-1A':'10-HNC10','IDF-1B':'10-HNC20','IDF-2B':'20-HNC20'
+      ,'FDF-1A':'10-HLB10'
+      ,'FDF-1B':'10-HLB20','FDF-2A':'20-HLB10','FDF-2B':'20-HLB20','PAF-1A':'10-HFD10','PAF-1B':'10-HFD20'
+      ,'PAF-2A':'20-HFD10','PAF-2B':'20-HFD20','APH-1A':'10-HLD10','APH-1B':'10-HLD20','APH-2A':'20-HLD10'
+      ,'APH-2B':'20-HLD20','MILL-1A':'10-HFV10','MILL-2A':'20-HFV10','MILL-1B':'10-HFV20','MILL-1C':'10-HFV30'
+      ,'MILL-2B':'20-HFV20','MILL-2C':'20-HFV30','MILL-1D':'10-HFV40','MILL-2D':'20-HFV40','MILL-2E':'20-HFV50'
+      ,'MILL-1E':'10-HFV50','MILL-1F':'10-HFV60','MILL-2F':'20-HFV60','MILL-2G':'20-HFV70','MILL-1G':'10-HFV70'
+      ,'MILL-2H':'20-HFV80','MILL-1H':'10-HFV80','TDBFP-1A':'10-LAA20','TDBFP-1B':'10-LAA30'
+      ,'TDBFP-2A':'20-LAA20','TDBFP-2B':'20-LAA30','U1 MDBFP':'10-LAA10','U2 MDBFP':'20-LAA10','U1 sootblowing system':'10-HCB51'
+      ,'U2 sootblowing system':'20-HCB51'}
   
 
 #st.subheader("Select the date range for notifications") 
@@ -69,24 +90,8 @@ def convert_df(df):
 #adding a download button to download csv file
 #st.download_button(label="Download",data=cs,file_name='Repeated notifications.csv',mime='text/csv')
 
-st.subheader("Select the Area/System/Equipment")
-options = st.multiselect('Select the Area/System/Equipment',['BLR-1','BLR-2','TG-1','TG-2','IDF-1A','IDF-1B','IDF-2A','IDF-2B','FDF-1A','FDF-1B','FDF-2A','FDF-2B','PAF-1A','PAF-1B','PAF-2A','PAF-2B'
-                                                     ,'APH-1A','APH-1B','APH-2A','APH-2B','MILL-1A','MILL-2A','MILL-1B','MILL-1C'
-                                                    ,'MILL-2B','MILL-2C','MILL-1D','MILL-2D','MILL-2E','MILL-1E','MILL-1F','MILL-2F'
-                                                    ,'MILL-2G','MILL-1G','TDBFP-1A','TDBFP-1B','TDBFP-2A','TDBFP-2B','MDBFP','U1 sootblowing system','U2 sootblowing system'])
-g=options[0]
-dict={'BLR-1':['10-HNC10','10-HNC20','10-HLB10','10-HLB20','10-HFD10','10-HFD20','10-HLD10','10-HLD20','10-HFV10','10-HFV20','10-HFV30','10-HFV40','10-HFV50','10-HFV60','10-HFV70','10-HFV80']
-      ,'BLR-2':['20-HNC10','20-HNC20','20-HLB10','20-HLB20','20-HFD10','20-HFD20','20-HLD10','20-HLD20','20-HFV10','20-HFV20','20-HFV30','20-HFV40','20-HFV50','20-HFV60','20-HFV70','20-HFV80']
-      ,'TG-1':'','TG-2':'','IDF-2A':'20-HNC10','IDF-1A':'10-HNC10','IDF-1B':'10-HNC20','IDF-2B':'20-HNC20'
-      ,'FDF-1A':'10-HLB10'
-      ,'FDF-1B':'10-HLB20','FDF-2A':'20-HLB10','FDF-2B':'20-HLB20','PAF-1A':'10-HFD10','PAF-1B':'10-HFD20'
-      ,'PAF-2A':'20-HFD10','PAF-2B':'20-HFD20','APH-1A':'10-HLD10','APH-1B':'10-HLD20','APH-2A':'20-HLD10'
-      ,'APH-2B':'20-HLD20','MILL-1A':'10-HFV10','MILL-2A':'20-HFV10','MILL-1B':'10-HFV20','MILL-1C':'10-HFV30'
-      ,'MILL-2B':'20-HFV20','MILL-2C':'20-HFV30','MILL-1D':'10-HFV40','MILL-2D':'20-HFV40','MILL-2E':'20-HFV50'
-      ,'MILL-1E':'10-HFV50','MILL-1F':'10-HFV60','MILL-2F':'20-HFV60','MILL-2G':'20-HFV70','MILL-1G':'10-HFV70'
-      ,'MILL-2H':'20-HFV80','MILL-1H':'10-HFV80','TDBFP-1A':'10-LAA20','TDBFP-1B':'10-LAA30'
-      ,'TDBFP-2A':'20-LAA20','TDBFP-2B':'20-LAA30','U1 MDBFP':'10-LAA10','U2 MDBFP':'20-LAA10','U1 sootblowing system':'10-HCB51'
-      ,'U2 sootblowing system':'20-HCB51'}
+
+
 data2=data[data['Functional Loc.'].str.contains(dict[g])]
 data2 = data2.drop(columns=['Notification','Order','Priority','User status','Req. start','Required End','Created By','System status','MaintenancePlan','Changed by'
                             ,'Changed On','MaintPlant','Reported by'])
