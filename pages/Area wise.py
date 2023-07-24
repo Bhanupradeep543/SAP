99bfd04e92dda1062cb8127c1bd38f149c3f9c8c
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2023-07-24 22:11:23 +0530

    Update and rename arewise.py to Area wise.py

--- pages/Area wise.py
+++ pages/Area wise.py
@@ -27,23 +27,11 @@ data=data.drop(data[data['Description'].isin(data1['Description'])].index)
 st.subheader('Total SAP notifications considered for analysis')
 st.subheader(data.shape[0])
 
-options = st.multiselect('select the Area/System/Equipment',['BLR-1','BLR-2','TG-1','TG-2','IDF-1A','IDF-1B','IDF-2A','IDF-2B','FDF-1A','FDF-1B','FDF-2A','FDF-2B','PAF-1A','PAF-1B','PAF-2A','PAF-2B'
-                                                     ,'APH-1A','APH-1B','APH-2A','APH-2B','MILL-1A','MILL-2A','MILL-1B','MILL-1C'
-                                                    ,'MILL-2B','MILL-2C','MILL-1D','MILL-2D','MILL-2E','MILL-1E','MILL-1F','MILL-2F'
-                                                    ,'MILL-2G','MILL-1G','TDBFP-1A','TDBFP-1B','TDBFP-2A','TDBFP-2B','MDBFP','U1 sootblowing system','U2 sootblowing system'])
+options = st.multiselect('select the Area/System/Equipment',['BLR-1','BLR-2','TG-1','TG-2'])
 g=options[0]
 dict={'BLR-1':['10-HNC10','10-HNC20','10-HLB10','10-HLB20','10-HFD10','10-HFD20','10-HLD10','10-HLD20','10-HFV10','10-HFV20','10-HFV30','10-HFV40','10-HFV50','10-HFV60','10-HFV70','10-HFV80']
       ,'BLR-2':['20-HNC10','20-HNC20','20-HLB10','20-HLB20','20-HFD10','20-HFD20','20-HLD10','20-HLD20','20-HFV10','20-HFV20','20-HFV30','20-HFV40','20-HFV50','20-HFV60','20-HFV70','20-HFV80']
-      ,'TG-1':'','TG-2':'','IDF-2A':'20-HNC10','IDF-1A':'10-HNC10','IDF-1B':'10-HNC20','IDF-2B':'20-HNC20'
-      ,'FDF-1A':'10-HLB10'
-      ,'FDF-1B':'10-HLB20','FDF-2A':'20-HLB10','FDF-2B':'20-HLB20','PAF-1A':'10-HFD10','PAF-1B':'10-HFD20'
-      ,'PAF-2A':'20-HFD10','PAF-2B':'20-HFD20','APH-1A':'10-HLD10','APH-1B':'10-HLD20','APH-2A':'20-HLD10'
-      ,'APH-2B':'20-HLD20','MILL-1A':'10-HFV10','MILL-2A':'20-HFV10','MILL-1B':'10-HFV20','MILL-1C':'10-HFV30'
-      ,'MILL-2B':'20-HFV20','MILL-2C':'20-HFV30','MILL-1D':'10-HFV40','MILL-2D':'20-HFV40','MILL-2E':'20-HFV50'
-      ,'MILL-1E':'10-HFV50','MILL-1F':'10-HFV60','MILL-2F':'20-HFV60','MILL-2G':'20-HFV70','MILL-1G':'10-HFV70'
-      ,'MILL-2H':'20-HFV80','MILL-1H':'10-HFV80','TDBFP-1A':'10-LAA20','TDBFP-1B':'10-LAA30'
-      ,'TDBFP-2A':'20-LAA20','TDBFP-2B':'20-LAA30','U1 MDBFP':'10-LAA10','U2 MDBFP':'20-LAA10','U1 sootblowing system':'10-HCB51'
-      ,'U2 sootblowing system':'20-HCB51'}
+      ,'TG-1':'','TG-2':''}
 #st.subheader("Select the date range for notifications") 
 #d = st.date_input("From", )
 #e = st.date_input("TO", )
@@ -78,13 +66,13 @@ def convert_df(df):
 #cs = convert_df(rp) 
 #adding a download button to download csv file
 #st.download_button(label="Download",data=cs,file_name='Repeated notifications.csv',mime='text/csv')
-data2=data[data['Functional Loc.'].str.contains(dict[g])]
+data2=data[data['Functional Loc.'].str.contains('|'.join(dict[g])]
 data2 = data2.drop(columns=['Notification','Order','Priority','User status','Req. start','Required End','Created By','System status','MaintenancePlan','Changed by'
                             ,'Changed On','MaintPlant','Reported by'])
 st.subheader("Total defects in the above System/equipment")
 rp=data2['System'].value_counts()
-st.subheader("TOP 5 repeated defects in the above System/equipment")
-rp=data2['System'].value_counts().head(5)
+st.subheader("TOP 20 repeated defects in the above System/equipment")
+rp=data2['System'].value_counts().head(20)
 st.write(rp)
 st.subheader("No.of defefcts planner group wise")
 fig, ax = plt.subplots()
@@ -101,34 +89,6 @@ st.bar_chart(data2['Created On'].value_counts().head(7))
 st.write(data2)
 cs = convert_df(data2) 
 st.download_button(label="Download",data=cs,file_name='Repeated notifications.csv',mime='text/csv')
-
-if g=='U1 sootblowing system':
-   data3=data2[data2['Description'].str.contains('struck|strucked|stucked|STRUCK|STUCK')]
-   data4=data2[data2['Description'].str.contains('overload|olr')]
-   data5=data2[data2['Description'].str.contains('leak|LEAK')]
-   data6=data2[data2['Description'].str.contains('lance|LANCE|tube')]
-   st.write("Sootblowers Srtucking defect")
-   st.write(data3['System'].value_counts())
-   st.write("Sootblowers overload defect")
-   st.write(data4['System'].value_counts())
-   st.write("Sootblowers flange leak")
-   st.write(data5['System'].value_counts())
-   st.write("Sootblowers lance tube defects")
-   st.write(data6['System'].value_counts())
-  
-elif g=='U2 sootblowing system':
-   data3=data2[data2['Description'].str.contains('struck|strucked|stucked|STRUCK|STUCK')]
-   data4=data2[data2['Description'].str.contains('overload|olr')]
-   data5=data2[data2['Description'].str.contains('leak|LEAK')]
-   data6=data2[data2['Description'].str.contains('lance|LANCE|tube')]
-   st.write("Sootblowers Srtucking defect")
-   st.write(data3['System'].value_counts())
-   st.write("Sootblowers overload defect")
-   st.write(data4['System'].value_counts())
-   st.write("Sootblowers flange leak")
-   st.write(data5['System'].value_counts())
-   st.write("Sootblowers lance tube defects")
-   st.write(data6['System'].value_counts())
 
 #adding a download button to download csv file
 
