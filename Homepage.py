5445f6ad418e2d3f0cba7fb47028d15b926ec552
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2023-07-25 21:09:28 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -6,6 +6,6 @@ st.set_page_config(
 )
 st.title("Main Page")
 st.subheader("SEIL SAP Notification analysis")
-st.subheader("Data condidered for analysis from Jan-2016 to May-2022")
+st.subheader("Data considered for analysis from Jan-2016 to May-2022")
 st.subheader("Total notifications considered for this analysis are: 97993")
 st.sidebar.success("Select a page above.")
