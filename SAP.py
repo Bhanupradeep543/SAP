076252dffbe419dbee357e0c05b8c3f468dd625f
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2025-10-23 22:45:50 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -46,6 +46,6 @@ if selected:
     # Number to multiply with
         multiplier = 1/520
     # Multiply second column and round to nearest integer
-        df['interval between each notification'] = (df['count'] * multiplier).round().astype(int)
+        df['interval between each notification'] = (df['Count'] * multiplier).round().astype(int)
         st.write(df)
         
