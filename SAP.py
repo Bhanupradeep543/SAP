f9116fc73f8b2e053196f463fbc679d5fba048e1
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2025-10-23 08:00:28 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -23,9 +23,6 @@ st.subheader(data.shape[0])
 options = st.multiselect('select the stage', ['STAGE-1', 'STAGE-2', 'STAGE-3'])                                                     
 g=options[0]
 dict={'STAGE-1':[S1COM],'STAGE-2':[S2COM],'STAGE-3':[S3COM]}
-#st.subheader("Select the date range for notifications") 
-#d = st.date_input("From", )
-#e = st.date_input("TO", )
 
 #column_name = 'System'
 #word_counts = data[column_name].value_counts()
@@ -37,26 +34,7 @@ dict={'STAGE-1':[S1COM],'STAGE-2':[S2COM],'STAGE-3':[S3COM]}
 #st.write(rp1)
 def convert_df(df):
  return df.to_csv().encode('utf-8')
-#cs = convert_df(repeated_rows) 
-#adding a download button to download csv file
-#st.download_button(label="Download",data=cs,file_name='Repeated notifications.csv',mime='text/csv')
-#st.subheader("Select the Planner group for obtaining repeated notifications")
-#options = st.multiselect('Select the planner Group',['CIA','CIB','CIC','CID','CIN','CIV','CNI','EAP','EBP','EBR','MAP','MBP','MBM','MTM'])
-#c=options[0]
-#st.write(c)
-#st.subheader("Max. notifications Reported by")
-#st.bar_chart(data['Reported by'].value_counts().head(10))
-#st.subheader("Max. notifications Planner group wise")
-#st.bar_chart(data['Planner group'].value_counts().head(7))
-#st.subheader("User status of notification")
-#st.write(data['User status'].value_counts().head())
-#st.subheader("Repeated notifications Planner group wise")
-#b=data[data['Planner group']==c]
-#rp=b['System'].value_counts().head(20)
-#st.write(rp)
-#cs = convert_df(rp) 
-#adding a download button to download csv file
-#st.download_button(label="Download",data=cs,file_name='Repeated notifications.csv',mime='text/csv')
+
 data2=data[data['Functional Loc.'].str.contains(dict[g])]
 data2 = data2.drop(columns=['Notification','Order','Priority','User status','Req. start','Required End','Created By','System status','MaintenancePlan','Changed by'
                             ,'Changed On','MaintPlant','Reported by'])
