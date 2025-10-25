db0d32d0c74113c58e97951a2bfe01f746f9cb67
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2025-10-25 19:23:07 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -16,7 +16,7 @@ from datetime import datetime
 
 st.subheader("""NTPC SAP Notifications """) # Tittle addition
 url = "https://raw.githubusercontent.com/Bhanupradeep543/SAP/master/korba total defects.xlsb"
-data = pd.read_excel(url)
+data = pd.read_excel(url,engine="pyxlsb")
 data['Notif.date'] = pd.to_datetime(data['Notif.date'], format='%Y%m%d')
 data1=data[data['Main WorkCtr']=='M400CWCT']
 st.subheader('Total SAP notifications considered for analysis')
