847bcaf4c560cfe4154a1275de607fcf6f21d443
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2025-10-25 22:29:36 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -58,3 +58,10 @@ if selected:
         yearly_count.rename(columns={'Notif.date': "gland leak"}, inplace=True)
         st.subheader("📅 Year-wise gland leaks")
         st.bar_chart(data=yearly_count, x="Year", y="gland leak")      
+        data4=data2[data2['Description'].str.contains('Vibration|vibration|VIBRATION')]
+        st.write("no.of vibrational issues in the selected stage",data4.shape[0])
+        data4["Year"] = data4['Notif.date'].dt.year
+        yearly_count = data4.groupby("Year")['Notif.date'].count().reset_index()
+        yearly_count.rename(columns={'Notif.date': "gland leak"}, inplace=True)
+        st.subheader("📅 Year-wise vibrational issues")
+        st.bar_chart(data=yearly_count, x="Year", y="vibrational issues")    
