1ef136c859e4fe8e6d29c65d140485d3b7bc969a
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2025-10-26 20:28:24 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,4 +1,4 @@
-import streamlit as st
+'''import streamlit as st
 
 st.set_page_config(
     page_title="Multipage App",
@@ -7,4 +7,4 @@ st.set_page_config(
 st.title("SEIL SAP Notification analysis")
 st.subheader("Data considered for analysis from Jan-2016 to May-2022")
 st.subheader("Total notifications considered for this analysis are: 97993")
-st.sidebar.success("Select a page above.")
+st.sidebar.success("Select a page above.")'''
