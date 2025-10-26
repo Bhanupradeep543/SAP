12dfc3a8ff4a4bdcd3e79fea89386340575f3424
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2025-10-26 20:40:44 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -13,29 +13,6 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import io
 from datetime import datetime
-# Set page config (optional)
-st.set_page_config(page_title="Custom Background", layout="wide")
-
-st.set_page_config(page_title="Custom Background", layout="wide")
-
-# CSS for background image
-st.markdown(
-    """
-    <style>
-    .stApp {
-        background-image: url("https://images.unsplash.com/photo-1506744038136-46273834b3fb");
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-st.title("Streamlit App with Custom Background")
-st.write("Background image should now be visible!")
 
 st.subheader("""NTPC SAP Notifications """) # Tittle addition
 url = "https://raw.githubusercontent.com/Bhanupradeep543/SAP/master/korba_defects.xlsx"
