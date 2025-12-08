9fd70c19b30fa275c5ede9b1ff2fe76a0f6dd7a4
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2025-12-08 23:45:23 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -11,24 +11,22 @@ import io
 from datetime import datetime
 import random 
 import base64
-# Initialize session state safely
-
 st.title("NTPC SAP Notifications Analysis")
 uploaded_file = st.file_uploader("Upload your defect data (Excel/CSV)",type=["xlsx", "xls", "csv"])
 if uploaded_file is not None:
-    # Read Excel or CSV safely
+    # Read data safely
     if uploaded_file.name.endswith(".csv"):
         data = pd.read_csv(uploaded_file)
     else:
         data = pd.read_excel(uploaded_file)
-
     st.success("File loaded successfully")
-data['Notif.date'] = pd.to_datetime(data['Notif.date'], format='%Y%m%d')
-    
-data1 = data[data['Main WorkCtr']=='M400CWCT']
-st.subheader('Total SAP notifications considered for analysis')
-st.subheader(data1.shape[0])
-    
+    data['Notif.date'] = pd.to_datetime(data['Notif.date'], format='%Y%m%d')
+    data1 = data[data['Main WorkCtr'] == 'M400CWCT']
+    st.subheader('Total SAP notifications considered for analysis')
+    st.subheader(data1.shape[0])
+
+else:
+    st.warning("Please upload your file to proceed.")
 st.subheader('Top 20 Repeated equipment notifications')
 data=data[data['equipment']!='KORBA STATION COMMON']
 repeat_defects = (data.groupby(['equipment']).size().reset_index(name='Count'))
