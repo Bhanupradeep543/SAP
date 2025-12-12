925ab8c606a3cfef7b6c03845fc2d77eed8ee204
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2025-12-12 20:25:10 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -155,7 +155,7 @@ if selected:
   st.bar_chart(data=yearly_count, x="Year", y="pump pressure issues")
 
   tc=data3.shape[0]+data4.shape[0]+data5.shape[0]+data6.shape[0]+data7.shape[0]+data8.shape[0]
-  +data9.shape[0]+data10.shape[0]+data11.shape[0]
+  +data9.shape[0]+data10.shape[0]+data11.shape[0]+data12.shape[0]
   per=(tc/data2.shape[0])*100
   per=int(per)
   st.write("% of notifications divided into various categories",per)   
