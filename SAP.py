38446cb23bcbce4abf4a823eff2c9b2ee29e0837
Bhanupradeep543 <115082091+Bhanupradeep543@users.noreply.github.com>
2025-12-13 00:10:49 +0530

    Update SAP.py

--- SAP.py
+++ SAP.py
@@ -53,7 +53,7 @@ df_valid = data1[data1["parent"].apply(is_valid_parent)]
 df_unique = df_valid.drop_duplicates(subset=["parent"])[["parent", EQUIP]]
 
 # Count appearances in the full master dataset
-appearance = df.groupby("parent").size().reset_index(name="Appearances")
+appearance = data1.groupby("parent").size().reset_index(name="Appearances")
 
 # Merge with unique mapping
 df_final = df_unique.merge(appearance, on="parent", how="left")
